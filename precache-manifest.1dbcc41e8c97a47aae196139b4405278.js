ce023430858b997c2bc1437e9f4684305d17b879
malerba423 <malerba423@gmail.com>
2020-10-22 14:08:28 -0500

    Updates

---
+++ precache-manifest.1dbcc41e8c97a47aae196139b4405278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcab221635604816bc85907f7dddfa0a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8373a438c6aa6054957",
+    "url": "/my-app/static/js/2.14e6cfe8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/my-app/static/js/2.14e6cfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/js/main.61bdfae7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
